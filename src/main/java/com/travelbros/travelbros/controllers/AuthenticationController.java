0ab5a62d3be57eedfa7fb47c2346a1ef51f40136
John Pedrotti <jdpedrotti@gmail.com>
2022-12-12 12:18:17 -0600

    fixing merge conflict

---
+++ src/main/java/com/travelbros/travelbros/controllers/AuthenticationController.java
@@ -0,0 +1,46 @@
+package com.travelbros.travelbros.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AuthenticationController {
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login_page/login_page";
+    }
+
+    @PostMapping("/login")
+    public String loginSubmission(){
+        return "redirect:/dashboard";
+    }
+
+    @GetMapping("/logout")
+    public String logout(){return "redirect:/login?logout";}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
