2a6b0ca6af868ca5d97eac5e20d3ea80b7d23ed8
John Pedrotti <jdpedrotti@gmail.com>
2022-12-12 14:36:39 -0600

    working on vehicle controller

--- src/main/java/com/travelbros/travelbros/controllers/VehicleController.java
+++ src/main/java/com/travelbros/travelbros/controllers/VehicleController.java
@@ -1,4 +1,30 @@
 package com.travelbros.travelbros.controllers;
 
+import com.travelbros.travelbros.repositories.UserRepository;
+import com.travelbros.travelbros.repositories.VehicleRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vehicles")
 public class VehicleController {
+
+
+// Dependency Injection
+
+    private final VehicleRepository vehicleDao;
+    private final UserRepository usersDao;
+
+
+// Constructor
+    public VehicleController(UserRepository usersDao, VehicleRepository vehicleDao) {
+        this.usersDao = usersDao;
+        this.vehicleDao = vehicleDao;
+    }
+
+
+
+
+
+
 }
