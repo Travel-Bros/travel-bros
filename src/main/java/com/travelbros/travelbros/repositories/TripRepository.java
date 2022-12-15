2dc54b9fd524b628faf55e3e7d4d8ea87b24a9de
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-12-15 14:35:18 -0600

    updating desending order

--- src/main/java/com/travelbros/travelbros/repositories/TripRepository.java
+++ src/main/java/com/travelbros/travelbros/repositories/TripRepository.java
@@ -1,10 +1,13 @@
 package com.travelbros.travelbros.repositories;
 
 import com.travelbros.travelbros.models.Trip;
+import com.travelbros.travelbros.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface TripRepository extends JpaRepository<Trip, Long> {
     Trip findById(long id);
 
-
+    public List<Trip> findAllByUserOrderByIdDesc(User user);
 }
--- src/main/java/com/travelbros/travelbros/repositories/UserRepository.java
+++ src/main/java/com/travelbros/travelbros/repositories/UserRepository.java
@@ -1,11 +1,15 @@
 package com.travelbros.travelbros.repositories;
 
+import com.travelbros.travelbros.models.Trip;
 import com.travelbros.travelbros.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findById(long id);
     User findByUsername(String username);
+    public List<Trip> findAllByOrderByIdDesc();
 }
