48f1a703522730c106732f15eb1419aa328ac1c7
Mason Wudtee <mason.wudtee@gmail.com>
2023-01-02 14:36:00 -0600

    adding basic calculator object

---
+++ src/main/java/com/travelbros/travelbros/models/Calculator.java
@@ -0,0 +1,28 @@
+package com.travelbros.travelbros.models;
+
+public class Calculator {
+
+
+    ///// Methods
+    public static double numberOfStops(double distance, double mpg, double tankSize) {
+        return (distance / (0.8* (mpg * tankSize)));
+    }
+    public static double remainingBudget(double maxBudget, double expenses, double numPpl) {
+        return (maxBudget - expenses) / numPpl;
+    }
+
+    public static double remainingBudget(double maxBudget, double expenses) {
+        return (maxBudget - expenses);
+    }
+
+    //// Constructor
+    public Calculator() {};
+
+
+
+    public static void main(String[] args) {
+        System.out.println(numberOfStops(400, 40, 10));
+        System.out.println(remainingBudget(800, 1120, 2));
+        System.out.println(remainingBudget(800, 1120));
+    }
+}
