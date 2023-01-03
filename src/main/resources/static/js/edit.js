8a100f913a366f8790c6d71a49913dd62bc218c5
Mason Wudtee <mason.wudtee@gmail.com>
2023-01-03 14:49:51 -0600

    new progress in edit. need to revisit google map api

--- src/main/resources/static/js/edit.js
+++ src/main/resources/static/js/edit.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
     // Event listener to redirect when .editButton clicked
     $(".editButton").on('click', function(e){
+        console.log("editButton clicked!");
         window.location.replace(`/trips/${$(this).attr("data-id")}/edit`);
     });
 });
 $(document).ready(function(){
     // Event listener to redirect when .deleteButton clicked
     $(".editVehicleButton").on('click', function(e){
+        console.log("editVehicleButton Clicked!");
         window.location.replace(`/vehicles/${$(this).attr("data-id")}/edit`);
     });
 });
