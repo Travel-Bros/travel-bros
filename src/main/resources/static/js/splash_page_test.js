e362b30658ddd6f84a8a0c35e915335a309d0dae
nightzillla <81122802+nightzillla@users.noreply.github.com>
2023-01-13 14:18:11 -0600

    updating splash page css

---
+++ src/main/resources/static/js/splash_page_test.js
@@ -0,0 +1,24 @@
+let path = document.querySelector('path');
+// let pathLength = document.getTotalLength()
+let pathLength = path.getTotalLength();
+
+path.style.strokeDasharray = pathLength + ' ' + pathLength;
+
+path.style.strokeDashoffset = pathLength;
+
+window.addEventListener('scroll', () => {
+    // What % is down is it?
+    var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+    // Length to offset the dashes
+    var drawLength = pathLength * scrollPercentage;
+
+    // Draw in reverse
+    path.style.strokeDashoffset = pathLength - drawLength;
+});
+//
+// let path = document.querySelector('path')
+// let pathLength = document.getTotalLength()
+//
+// path.style.strokeDasharray = pathLength + ' ' + pathLength;
+//
+// path.style.strokeDashoffset = pathLength;
